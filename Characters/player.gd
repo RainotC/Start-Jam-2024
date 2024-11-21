6de7c055456d0ee88d164265cc1526bc2a046085
rainot <olga24@onet.pl>
2024-11-21 04:58:06 +0100

    Input from controller

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,31 +1,47 @@
 extends CharacterBody2D
 
-@export var speed : float = 100
+@export var player_index = 1
+@export var speed : float = 5
+var input_velocity = Vector2.ZERO
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
 var pressed_before = "down"
 
+func _ready():
+	for device_id in Input.get_connected_joypads():
+		print("Connected device: ", Input.get_joy_name(device_id), " with ID: ", device_id)
+		
+func _input(event):
+	if event is InputEventJoypadButton:
+		print("Button pressed: ", event.button_index, " on device: ", event.device)
+		
+
+		
+
 func _physics_process(_delta):
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+	input_velocity = Vector2.ZERO
 	
-	velocity = input_direction * speed
-	if Input.is_action_pressed("right"):
+	
+	if Input.is_joy_button_pressed(player_index, 15) or Input.is_action_pressed("right", player_index):
+		input_velocity.x=1
 		_animated_sprite.play("MoveRight")
-		pressed_before = "right"
-	elif Input.is_action_pressed("left"):
+#		pressed_before = "right"
+	elif Input.is_joy_button_pressed(player_index, 14) or Input.is_action_pressed("left", player_index):
+		input_velocity.x= -1
 		_animated_sprite.play("MoveLeft")
-		pressed_before = "left"
-	elif Input.is_action_pressed("up"):
+#		pressed_before = "left"
+	elif Input.is_joy_button_pressed(player_index, 12) or Input.is_action_pressed("up", player_index):
+		input_velocity.y= -1
 		_animated_sprite.play("MoveUp")
-		pressed_before = "up"
-	elif Input.is_action_pressed("down"):
+#		pressed_before = "up"
+	elif Input.is_joy_button_pressed(player_index, 13) or Input.is_action_pressed("down", player_index):
+		input_velocity.y= 1
 		_animated_sprite.play("MoveDown")
-		pressed_before = "down"
-	elif pressed_before=="up":
+#		pressed_before = "down"
+	elif input_velocity == Vector2.ZERO:
+		_animated_sprite.play("IdleRight")
+	"""elif pressed_before=="up":
 		_animated_sprite.play("IdleUp")
 	elif pressed_before=="left":
 		_animated_sprite.play("IdleLeft")	
@@ -33,7 +49,7 @@ func _physics_process(_delta):
 	elif pressed_before=="right":
 		_animated_sprite.play("IdleRight")	
 	else:
-		_animated_sprite.play("IdleDown")
-	
-	print(pressed_before)
+		_animated_sprite.play("IdleDown")"""
+		
+	velocity += input_velocity*speed 
 	move_and_slide()
